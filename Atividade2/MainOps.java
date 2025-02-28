ecd041ef9f80543c416b2112563b71aff975a61e
Iure Costa <131163902+Iurexz@users.noreply.github.com>
2025-02-27 22:57:42 -0300

    i1

---
+++ Atividade2/MainOps.java
@@ -0,0 +1,8 @@
+package Atividade2;
+
+public class MainOps {
+    public static void main(String[] args) {
+        Soma somar = new Soma();
+        System.out.println(somar.calcular(20, 30));
+    }
+}
