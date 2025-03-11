546018c81396a1490581fa8d75ac0fbffa50b8be
Iure Costa <131163902+Iurexz@users.noreply.github.com>
2025-03-11 20:41:40 -0300

    .,

--- Atividade3/Funcionario.java
+++ Atividade3/Funcionario.java
@@ -8,53 +8,76 @@ public abstract class Funcionario {
     protected double salarioBase;
 
     
-    public Funcionario(String nome, String dataNasc, Sexo sexo, Setor setor, double salarioBase) {
-        this.nome = nome;
-        this.dataNasc = dataNasc;
-        this.sexo = sexo;
-        this.setor = setor;
-        this.salarioBase = salarioBase;
     
 
-public String getNome() {
-    return nome;
-}
+    public String getNome() {
+        return nome;
+    }
 
-public void setNome(String nome) {
-    this.nome = nome;
-}
 
-public String getDataNasc() {
-    return dataNasc;
-}
 
-public void setDataNasc(String dataNasc) {
-    this.dataNasc = dataNasc;
-}
 
-public Sexo getSexo() {
-    return sexo;
-}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-public void setSexo(Sexo sexo) {
-    this.sexo = sexo;
-}
 
-public Setor getSetor() {
-    return setor;
-}
 
-public void setSetor(Setor setor) {
-    this.setor = setor;
-}
 
-public double getSalarioBase() {
-    return salarioBase;
-}
+    public String getDataNasc() {
+        return dataNasc;
+    }
 
-public void setSalarioBase(double salarioBase) {
-    this.salarioBase = salarioBase;
-}}
+
+
+
+    public void setDataNasc(String dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+
+
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+
+
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+
+
+
+    public Setor getSetor() {
+        return setor;
+    }
+
+
+
+
+    public void setSetor(Setor setor) {
+        this.setor = setor;
+    }
+
+
+
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+
+
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+
 
 
     public Funcionario(String nome, String dataNasc, Sexo sexo, Setor setor, double salarioBase) {
@@ -64,6 +87,18 @@ public void setSalarioBase(double salarioBase) {
         this.setor = setor;
         this.salarioBase = salarioBase;
     }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Funcionario [nome=" + nome + ", dataNasc=" + dataNasc + ", sexo=" + sexo + ", setor=" + setor
+                + ", salarioBase=" + salarioBase + "]";
+    }
+
+
+
 
     public abstract double getSalarioFinal();
 
